31869ec68e4ed6e05d630c3b013b71fa88e33158
TKWA-PADX1E-LPT\Oreo <okakm.k.h30@gmail.com>
2022-06-10 16:19:03 +0900

    日本全国都道府県市区町村別人口データCSVと人口データ取得・表示するpythonファイルを作成

---
+++ popul-dashboard_v_1_0.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+"""
+    Japan's Population Dashboard on PyQt5 Python3.80
+    System Development A 2022/05/20
+
+    quote :
+    Window : https://www.sejuku.net/blog/75467, https://teratail.com/questions/150883
+    Widgets,Graphics :https://qiita.com/kenasman/items/73d01df973a25ae704e4
+    MsgBox : https://webbibouroku.com/Blog/Article/qgis3-python-messagebox , https://doc.qt.io/qtforpython/PySide2/QtWidgets/QMessageBox.html
+"""
+
+
+def fetchAllPref():
+    """
+    各都道府県の人口を返す
+    :return:
+    """
