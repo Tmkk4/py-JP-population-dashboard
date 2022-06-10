c70e13d689d1d2c69766595c24ae9ce2108039f2
TKWA-PADX1E-LPT\Oreo <okakm.k.h30@gmail.com>
2022-06-10 19:13:06 +0900

    CSVファイルから全国及び各都道府県レベルの人口データレコードを取り出す部分まで

--- popul-dashboard_v_1_0.py
+++ popul-dashboard_v_1_0.py
@@ -8,10 +8,26 @@
     Widgets,Graphics :https://qiita.com/kenasman/items/73d01df973a25ae704e4
     MsgBox : https://webbibouroku.com/Blog/Article/qgis3-python-messagebox , https://doc.qt.io/qtforpython/PySide2/QtWidgets/QMessageBox.html
 """
-
+import re
 
 def fetchAllPref():
     """
-    各都道府県の人口を返す
+    人口データCSVファイルから都道府県レベルでの人口データを抽出し, 各都道府県の人口を返す
+
+    CSVの1行 : エリアコード 01000,エリア名,総人口,男性人口,女性人口
     :return:
     """
+    prefs = []  # 都道府県別人口レコードの集合
+    f = open('FEH_00200521_220610143730.csv','r')
+    records = f.readlines() # CSVファイルの内容を 1行毎に改行コードありで取得
+
+    # 正規表現パターンで エリアコード上2桁が01～47 かつ 残りの3桁が000の行を表現する
+    # 全国及び都道府県の人口レコードのみ抽出
+    ptn = re.compile(r'/d{2}(000)')  # 任意の数字2桁の直後に000が続く文字列にマッチ
+    for p in records:
+        # CSVファイル内の1行ずつに対して処理
+        flg = re.search(ptn, p)
+        if flg is not None:
+            prefs += p  # 都道府県レコードであれば 追加し保持しておく
+
+    print(prefs)
